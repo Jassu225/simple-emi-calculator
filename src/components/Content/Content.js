5ecfcddf72405a67e48d53e49dbcd9600dae61b1
Jaswanth Sai Sattenapalli <jaswanthsaisattenapalli@gmail.com>
2019-09-12 12:29:06 +0530

    minor UI update

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -9,16 +9,18 @@ class Content extends Component {
     const totalAmount = (this.props.monthlyPayment * (this.props.isFetchingData ? this.props.lastLoanDuration : this.props.loanDuration));
     return (
       <div className="content">
-        <div className="main-header">EMI Details</div>
-        <div className="interest-rate header">Interest Rate:</div>
-        <div className="interest-rate value">{this.props.interestRate}</div>
-        <div className="monthly-payment header">Monthly Payment:</div>
-        <div className="monthly-payment value">
-          {this.props.monthlyPayment + " " + this.props.currencyUnit}
-        </div>
-        <div className="total-payment header">Total Amount Payable:</div>
-        <div className="total-payment value">
-          {totalAmount + " " + this.props.currencyUnit}
+        <div className="main-header">EMI Info</div>
+        <div className="wrapper">
+          <div className="interest-rate header">Interest Rate:</div>
+          <div className="interest-rate value">{this.props.interestRate}</div>
+          <div className="monthly-payment header">Monthly Payment:</div>
+          <div className="monthly-payment value">
+            {this.props.monthlyPayment + " " + this.props.currencyUnit}
+          </div>
+          <div className="total-payment header">Total Amount Payable:</div>
+          <div className="total-payment value">
+            {totalAmount + " " + this.props.currencyUnit}
+          </div>
         </div>
       </div>
     );
