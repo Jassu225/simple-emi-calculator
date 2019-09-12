4122ca2866b36b8c617eafb4188671076e00d9d5
Jaswanth Sai Sattenapalli <jaswanthsaisattenapalli@gmail.com>
2019-09-12 12:45:09 +0530

    first build push

---
+++ build/precache-manifest.c78fa5b5b93f3beb7938041371e911eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d390abe0c4a58a521b80c9ee6bc4da",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7eba84f41f4d1073d18c",
+    "url": "/static/css/main.73dd9324.chunk.css"
+  },
+  {
+    "revision": "c5f9dc9f5e2a02c1c5e2",
+    "url": "/static/js/2.86082fab.chunk.js"
+  },
+  {
+    "revision": "7eba84f41f4d1073d18c",
+    "url": "/static/js/main.431646e7.chunk.js"
+  },
+  {
+    "revision": "c0c7aca1087f4ce13447",
+    "url": "/static/js/runtime~main.f59bec8c.js"
+  }
+]);
